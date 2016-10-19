9c975f4f5cef9985fa0ddf51fa0805da100274c6
ppatierno <ppatierno@live.com>
2016-10-19 15:22:50 +0200

    Added example about running the MQTT server in verticles (btw to run example the #2 is needed)

---
+++ src/main/java/io/vertx/mqtt/examples/verticle/MqttServerVerticle.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.vertx.mqtt.examples.verticle;
+
+import io.netty.handler.codec.mqtt.MqttConnectReturnCode;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.mqtt.MqttServer;
+import io.vertx.mqtt.MqttServerOptions;
+
+/**
+ * An example of using the MQTT server as a verticle
+ */
+public class MqttServerVerticle extends AbstractVerticle {
+
+  private static final Logger log = LoggerFactory.getLogger(MqttServerVerticle.class);
+
+  private MqttServer server;
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+
+    MqttServerOptions options = new MqttServerOptions();
+    options.setHost("0.0.0.0").setPort(1883);
+
+    this.server = MqttServer.create(this.vertx, options);
+
+    this.server.endpointHandler(endpoint -> {
+
+      log.info("connected client " + endpoint.clientIdentifier());
+
+      endpoint.writeConnack(MqttConnectReturnCode.CONNECTION_ACCEPTED, false);
+    });
+
+    this.server.listen(ar -> {
+
+      if (ar.succeeded()) {
+        log.info("MQTT server started and listening on port " + ar.result().actualPort());
+      } else {
+        log.info("MQTT server error on start", ar.cause());
+      }
+
+    });
+
+  }
+}
