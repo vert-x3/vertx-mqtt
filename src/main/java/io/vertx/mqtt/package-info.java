ce400caacdbd90a15ceaa9a98001b6e6250d5603
ppatierno <ppatierno@live.com>
2016-12-24 12:15:18 +0100

    Added first information about Vert.x MQTT server and how to use it for doc generation

--- src/main/java/io/vertx/mqtt/package-info.java
+++ src/main/java/io/vertx/mqtt/package-info.java
@@ -14,5 +14,57 @@
  * limitations under the License.
  */
 
-@io.vertx.codegen.annotations.ModuleGen(name = "vertx-mqtt-server", groupPackage = "io.vertx")
+/**
+ * = Vert.x MQTT server
+ *
+ * This component provides a server which is able to handle connections, communication and messages exchange with remote
+ * link:http://mqtt.org/[MQTT] clients. Its API provides a bunch of events related to raw protocol messages received by
+ * clients and exposes some features in order to send messages to them.
+ *
+ * It's not a fully featured MQTT broker but can be used for building something like that or for protocol translation.
+ *
+ * WARNING: this module has the tech preview status, this means the API can change between versions.
+ *
+ * == Using Vert.x MQTT server
+ *
+ * As component not yet officially released in the Vert.x stack, to use the Vert.x MQTT server current snapshot version,
+ * add the following repository under the _repositories_ section and the following dependency to the _dependencies_ section
+ * of your build descriptor:
+ *
+ * * Maven (in your `pom.xml`):
+ *
+ * [source,xml,subs="+attributes"]
+ * ----
+ * <repository>
+ *     <id>oss.sonatype.org-snapshot</id>
+ *     <url>https://oss.sonatype.org/content/repositories/snapshots</url>
+ * </repository>
+ * ----
+ *
+ * [source,xml,subs="+attributes"]
+ * ----
+ * <dependency>
+ *     <groupId>io.vertx</groupId>
+ *     <artifactId>vertx-mqtt-server</artifactId>
+ *     <version>3.4.0-SNAPSHOT</version>
+ * </dependency>
+ * ----
+ *
+ * * Gradle (in your `build.gradle` file):
+ *
+ * [source,groovy,subs="+attributes"]
+ * ----
+ * maven { url "https://oss.sonatype.org/content/repositories/snapshots" }
+ * ----
+ *
+ * [source,groovy,subs="+attributes"]
+ * ----
+ * compile io.vertx:vertx-mqtt-server:3.4.0-SNAPSHOT
+ * ----
+ */
+@Document(fileName = "index.adoc")
+@ModuleGen(name = "vertx-mqtt-server", groupPackage = "io.vertx")
 package io.vertx.mqtt;
+
+import io.vertx.codegen.annotations.ModuleGen;
+import io.vertx.docgen.Document;
