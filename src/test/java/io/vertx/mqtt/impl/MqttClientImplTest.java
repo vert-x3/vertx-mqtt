a7cd7f93d4d9f3cb4f46a70c9ae60940009f3a20
Kai Hudalla <sophokles.kh@gmail.com>
2020-09-15 12:22:12 +0200

    Fix copyright header
    This has been worked on during my spare time.
    
    Signed-off-by: Kai Hudalla <sophokles.kh@gmail.com>

--- src/test/java/io/vertx/mqtt/impl/MqttClientImplTest.java
+++ src/test/java/io/vertx/mqtt/impl/MqttClientImplTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Bosch.IO GmbH
+ * Copyright 2020 Kai Hudalla
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
