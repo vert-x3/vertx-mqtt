9d3729d1173b6eaed3cfdf0f5eef5d010dd15e77
Jens Reimann <jreimann@redhat.com>
2021-05-18 14:29:59 +0200

    Rerun codegen
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- src/main/generated/io/vertx/mqtt/reconnect/ConstantReconnectDelayOptionsConverter.java
+++ src/main/generated/io/vertx/mqtt/reconnect/ConstantReconnectDelayOptionsConverter.java
@@ -1,13 +1,17 @@
 package io.vertx.mqtt.reconnect;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.impl.JsonUtil;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter and mapper for {@link io.vertx.mqtt.reconnect.ConstantReconnectDelayOptions}.
- * NOTE: This class has been automatically generated from the {@link io.vertx.mqtt.reconnect.ConstantReconnectDelayOptions}
- * original class using Vert.x codegen.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.mqtt.reconnect.ConstantReconnectDelayOptions} original class using Vert.x codegen.
  */
 public class ConstantReconnectDelayOptionsConverter {
+
 
   public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ConstantReconnectDelayOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
--- src/main/generated/io/vertx/mqtt/reconnect/ExponentialBackoffDelayOptionsConverter.java
+++ src/main/generated/io/vertx/mqtt/reconnect/ExponentialBackoffDelayOptionsConverter.java
@@ -1,13 +1,17 @@
 package io.vertx.mqtt.reconnect;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.impl.JsonUtil;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter and mapper for {@link io.vertx.mqtt.reconnect.ExponentialBackoffDelayOptions}.
- * NOTE: This class has been automatically generated from the {@link io.vertx.mqtt.reconnect.ExponentialBackoffDelayOptions}
- * original class using Vert.x codegen.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.mqtt.reconnect.ExponentialBackoffDelayOptions} original class using Vert.x codegen.
  */
 public class ExponentialBackoffDelayOptionsConverter {
+
 
   public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ExponentialBackoffDelayOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
