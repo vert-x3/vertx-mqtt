8a628271ff9b3657c4f958dc144e5fc69317fced
A.Fink <aprpda@gmail.com>
2023-09-12 15:00:29 +0300

    ConcurrentHashSet is redundant with ConcurrentHashMap#newKeySet() #4851

--- src/test/java/io/vertx/mqtt/test/server/MqttServerTest.java
+++ src/test/java/io/vertx/mqtt/test/server/MqttServerTest.java
@@ -18,7 +18,6 @@ package io.vertx.mqtt.test.server;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.impl.ConcurrentHashSet;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.ext.unit.TestContext;
@@ -81,7 +80,7 @@ public class MqttServerTest {
     int numConnections = numServers * 20;
 
     List<MqttServer> servers = new ArrayList<>();
-    Set<MqttServer> connectedServers = new ConcurrentHashSet<>();
+    Set<MqttServer> connectedServers = ConcurrentHashMap.newKeySet();
 
     CountDownLatch latchListen = new CountDownLatch(numServers);
     CountDownLatch latchConns = new CountDownLatch(numConnections);
